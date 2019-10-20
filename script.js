201a6b18de8b9a79bc8ee788637885c2429c7f23
Hive51 <50019416+Hive51@users.noreply.github.com>
2019-10-20 14:24:05 +0200

    Update of script addEventListener scroll

--- script.js
+++ script.js
@@ -118,7 +118,58 @@ for (let i = 0; i < wildersArray.length; i++){
     picProfile.classList.add("profile-pic"); 
 };
 
+const navigationPanel = document.getElementById("navigation-panel");
 
+for (let i = 0; i < wildersArray.length; i++) {
+    let aTag = document.createElement("a");
+    aTag.id = wildersArray[i].name + "-link";
+    aTag.classList.add("nav-menu-item");
+    aTag.title=wildersArray[i].name;
+    aTag.href="#" + wildersArray[i].name;
+    aTag.innerHTML = "#" + (i >= 9 ? i + 1 : "0" + (i + 1)) + " " + wildersArray[i].name;
+    navigationPanel.appendChild(aTag);    
+}
+
+var navPanel = document.getElementById("navigation-panel");
+/*
+
+1) Répcupérer l'event scroll // scroll vers le haut, scroll vers le vas
+*/
+i = 0;
+var lastScrollTop = 0;
+window.addEventListener("scroll", () => {
+
+    console.log("je suis scrollé");
+    var scrollingPosition = window.pageYOffset;
+    if (scrollingPosition > lastScrollTop) {
+        console.log("LastScrollTop :" + lastScrollTop);
+        console.log("ScrollingPosition :" + scrollingPosition)
+            //scrolling down
+        wildersArray[i].onFocus = true;
+        //wildersArray[i + 1].onFocus = false;
+        i <= 0 ? i = 2 : i = i - 1;
+    } else if (scrollingPosition < lastScrollTop) {
+        console.log("scroll up")
+        console.log("LastScrollTop :" + lastScrollTop);
+        console.log("ScrollingPosition :" + scrollingPosition)
+            //scrolling up
+        wildersArray[i].onFocus = true;
+        //wildersArray[i + 1].onFocus = false;
+        i >= 2 ? i = 0 : i = i + 1;
+    }
+    lastScrollTop = scrollingPosition <= 0 ? 0 : scrollingPosition; // For Mobile or negative scrolling
+
+    
+});
+
+/*
+
+2) Dans mon array wildersArray, on rentre dans l'array d'objet 
+
+
+quand je scroll, propriété de Focus On = true et quand je scroll, off et le prochain off. 
+
+3)  Donc créer une classe qui toggle sur la couleur */
 
 
 
